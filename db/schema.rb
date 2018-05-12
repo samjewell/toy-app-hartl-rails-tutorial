ab28c52a050bf425856899312023442a00c22dcf
Sam Jewell <sam@bridge-u.com>
2018-05-12 18:37:44 +0100

    Add avatar_url column to User table

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818172329) do
+ActiveRecord::Schema.define(version: 20180512173656) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -25,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150818172329) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "avatar_url"
   end
 
 end
